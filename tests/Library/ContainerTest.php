d925b6ede1b0ce9517f4e7b80d4fb60fa79f2a2d
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-04-12 17:13:21 -0400

    Making sure that the config gets built right.  Fixes CWGEN-22

--- tests/Library/ContainerTest.php
+++ tests/Library/ContainerTest.php
@@ -9,7 +9,7 @@ use Tests\Spinen\ConnectWise\BaseTest;
  * Class ContainerTest
  *
  * @package Tests\Spinen\ConnectWise\Library
- * @group library
+ * @group   library
  */
 class ContainerTest extends BaseTest
 {
@@ -45,6 +45,36 @@ class ContainerTest extends BaseTest
     public function it_will_return_the_instance_of_the_container()
     {
         $this->assertInstanceOf('Spinen\\ConnectWise\\Library\\Container', Container::getInstance());
+    }
+
+    /**
+     * @test
+     */
+    public function it_converts_the_options_array_to_a_config_object()
+    {
+        $this->assertInstanceOf('Spinen\\ConnectWise\\Library\\Config', $this->container->get('config'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_stores_the_passed_in_options_under_config_key()
+    {
+        $this->assertEquals("value", $this->container->get('config')['key']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_store_the_di_array_in_the_config()
+    {
+        $options = $this->buildConfigs();
+
+        $options['di'] = ['alias' => ['class' => 'stdClass']];
+
+        $container = new Container($options);
+
+        $this->assertArrayNotHasKey('di', $container->get('config'));
     }
 
     /**
