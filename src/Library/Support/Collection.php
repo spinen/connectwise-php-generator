aeaa7c85dcdf398efb564f69dfdf816aa1e419cf
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-04-13 21:49:12 -0400

    Wrap laravel's collection into a collection that we can add some methods

---
+++ src/Library/Support/Collection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Spinen\ConnectWise\Library\Support;
+
+use Illuminate\Support\Collection as IlluminateCollection;
+
+class Collection extends IlluminateCollection
+{
+
+    // Place holder class in case we need to mix in our own methods
+
+}
