3faa167b62d758dc52e057625c402e9b30c2e216
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-05-02 18:43:18 -0400

    Verifying that we have a getter on the collection

--- tests/Library/Results/CollectionTest.php
+++ tests/Library/Results/CollectionTest.php
@@ -26,7 +26,9 @@ class CollectionTest extends BaseTest
     {
         parent::setUp();
 
-        $this->collection = new Collection();
+        $this->collection = new Collection([
+            'key' => 'value',
+        ]);
     }
 
     /**
@@ -35,6 +37,23 @@ class CollectionTest extends BaseTest
     public function it_can_be_constructed()
     {
         $this->assertInstanceOf('Spinen\\ConnectWise\\Library\\Results\\Collection', $this->collection);
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_the_keys_as_a_property()
+    {
+        $this->assertEquals($this->collection['key'], $this->collection->key);
+    }
+
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function it_raises_exception_when_asking_for_a_property_that_there_is_not_a_key()
+    {
+        $this->collection->does_not_exists;
     }
 
 }
