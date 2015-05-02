f2c7e19a63dfb773d84fd9eebe5b0bc956f8ca13
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-05-02 19:41:56 -0400

    Checking for getter.  Fixes CWGEN-37

--- src/Library/Results/ValueObject.php
+++ src/Library/Results/ValueObject.php
@@ -7,6 +7,35 @@ use Spinen\ConnectWise\Library\Support\Collection as BaseCollection;
 class ValueObject extends BaseCollection
 {
 
-    // Place holder class in case we need to mix in our own methods
+    /**
+     * Get value from getter or from the collection by key.
+     *
+     * @param  mixed $key
+     * @param  mixed $default
+     *
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        $getter = 'get' . studly_case($key) . 'Attribute';
+
+        if (method_exists($this, $getter)) {
+            return $this->{$getter}();
+        }
+
+        return parent::get($key, $default);
+    }
+
+    /**
+     * Get an item at a given offset.
+     *
+     * @param mixed $key
+     *
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->get($key);
+    }
 
 }
