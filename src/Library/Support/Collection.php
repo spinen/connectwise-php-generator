afa6f3a5bf7b9104f540bc761eb2770f6736c1fc
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-05-02 09:11:33 -0400

    Getter for the collection.  Fixes CWGEN-36

--- src/Library/Support/Collection.php
+++ src/Library/Support/Collection.php
@@ -3,10 +3,39 @@
 namespace Spinen\ConnectWise\Library\Support;
 
 use Illuminate\Support\Collection as IlluminateCollection;
+use InvalidArgumentException;
 
+/**
+ * Class Collection
+ *
+ * @package Spinen\ConnectWise\Library\Support
+ */
 class Collection extends IlluminateCollection
 {
 
+    /**
+     * Getter to make the object fell more like an object
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    function __get($name)
+    {
+        if ($this->has($name)) {
+            return $this->get($name);
+        }
+
+        throw new InvalidArgumentException(sprintf("Undefined property: %s", $name));
+    }
+
+    /**
+     * Calculate the average for a value in the collection
+     *
+     * @param string $value
+     *
+     * @return float|void
+     */
     public function average($value)
     {
         $count = $this->count();
