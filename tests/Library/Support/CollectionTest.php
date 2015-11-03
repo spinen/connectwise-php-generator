f4d1940b082d7a5b342b49e2c3f6622e51ba710f
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-11-02 20:13:55 -0500

    Now that Laravel has an average method, don't need ours

--- tests/Library/Support/CollectionTest.php
+++ tests/Library/Support/CollectionTest.php
@@ -37,27 +37,4 @@ class CollectionTest extends BaseTest
         $this->assertInstanceOf('Spinen\\ConnectWise\\Library\\Support\\Collection', $this->collection);
     }
 
-    /**
-     * @test
-     */
-    public function it_calculates_average()
-    {
-        $this->collection->push(new ValueObject(['property' => 10]));
-        $this->collection->push(new ValueObject(['property' => 20]));
-
-        $expected = 15;
-
-        $this->assertEquals($expected, $this->collection->average('property'));
-    }
-
-    /**
-     * @test
-     */
-    public function it_does_not_try_to_divide_by_zero_when_calculating_average_with_properties()
-    {
-        $expected = null;
-
-        $this->assertEquals($expected, $this->collection->average('property'));
-    }
-
 }
