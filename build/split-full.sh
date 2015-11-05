bbfae7fafeacb19d136ed58183b8b99a2581bc54
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-11-05 16:46:06 -0500

    Copy/paste error in changing to github uri's

--- build/split-full.sh
+++ build/split-full.sh
@@ -2,7 +2,7 @@
 
 git subsplit init git@github.com:spinen/connectwise-php-library-generator.git
 
-git subsplit publish --heads="master develop" src/Client:gitlab@git@github.com:spinen/connectwise-php-client.git
-git subsplit publish --heads="master develop" src/Library:gitlab@git@github.com:spinen/connectwise-php-library.git
+git subsplit publish --heads="master develop" src/Client:git@github.com:spinen/connectwise-php-client.git
+git subsplit publish --heads="master develop" src/Library:git@github.com:spinen/connectwise-php-library.git
 
 rm -rf .subsplit/
