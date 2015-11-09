526c59247f09053a8a216e3bbe01a237d8b136c1
Jimmy Puckett <jimmy.puckett@spinen.com>
2015-11-09 08:59:29 -0500

    Stub out a temp metadata class until CW decides what to do about the empty property

---
+++ src/Library/Models/Metadata.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Spinen\ConnectWise\Library\Models;
+
+use ArrayAccess;
+
+class Metadata implements ArrayAccess
+{
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @var string[]
+     */
+    static $swaggerTypes = [];
+
+    /**
+     * Array of attributes where the key is the local name, and the value is the original name
+     *
+     * @var string[]
+     */
+    static $attributeMap = [];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    static $setters = [];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    static $getters = [];
+
+    public function __construct(array $data = [])
+    {
+        foreach ($data as $key => $value) {
+            $this->{snake_case($key)} = $value;
+        }
+    }
+
+    /**
+     * Whether a offset exists
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     *                      </p>
+     *
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->$offset);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     *                      </p>
+     *
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    /**
+     * Offset to set
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to assign the value to.
+     *                      </p>
+     * @param mixed $value  <p>
+     *                      The value to set.
+     *                      </p>
+     *
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->$offset = $value;
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @link  http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to unset.
+     *                      </p>
+     *
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->$offset);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        if (defined('JSON_PRETTY_PRINT')) {
+            return json_encode(get_object_vars($this), JSON_PRETTY_PRINT);
+        } else {
+            return json_encode(get_object_vars($this));
+        }
+    }
+}
